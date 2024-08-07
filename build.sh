19217ec7acce6cfb1ea04b402c53be113a25a95e
RodrigoHC87 <144636768+RodrigoHC87@users.noreply.github.com>
2024-08-06 22:54:59 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 
-#- Comentamos esto, ya que no usamos poetry.
-#pip install -r requirements.txt
+pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
